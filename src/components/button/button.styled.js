3b099a231b62b1c9da6fdb1dfd5264a05bb441b6
ludin20 <oswellgrundmeier42@gmail.com>
2022-01-22 06:51:56 -0800

    initial commit

---
+++ src/components/button/button.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+export const ButtonContainer = styled.div`
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
+  font-size: ${(props) => props.fsize}px;
+  background-color: #3db97b;
+  border-radius: 30px;
+  color: white;
+  display: flex;
+  text-transform: uppercase;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Routes , Route} from 'react-router-dom';
+// import Home from '../pages/Home';
+
+const Router = () =>{
+	return(
+		<Routes>
+			
+		</Routes>
+	);
+};
+
+export default Router;
